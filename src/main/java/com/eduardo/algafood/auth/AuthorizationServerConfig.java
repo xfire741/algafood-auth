588de578735a499e49463533e50c37b9dfcb39f1
xfire741 <eduardovictor28@gmail.com>
2023-09-20 11:16:10 -0300

    Configurando o RedisTokenStore

--- src/main/java/com/eduardo/algafood/auth/AuthorizationServerConfig.java
+++ src/main/java/com/eduardo/algafood/auth/AuthorizationServerConfig.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,6 +15,8 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.CompositeTokenGranter;
 import org.springframework.security.oauth2.provider.TokenGranter;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
@@ -27,6 +30,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	
 	@Autowired
 	private AuthenticationManager authenticationManager;
+	
+	@Autowired
+	private RedisConnectionFactory connectionFactory;
 	
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
@@ -86,8 +92,13 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		endpoints.authenticationManager(authenticationManager)
 		.userDetailsService(userDetailsService)
+		.tokenStore(redisTokenStore())
 		.reuseRefreshTokens(false)
 		.tokenGranter(tokenGranter(endpoints));
+	}
+	
+	private TokenStore redisTokenStore() {
+		return new RedisTokenStore(connectionFactory);
 	}
 	
 }
